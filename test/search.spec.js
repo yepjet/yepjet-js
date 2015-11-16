4c6cef20b3c4f33f0b9936546eebf60e42eb1bd3
Riccardo Gueli Alletti <riccardo.gueli.alletti@gmail.com>
2015-11-16 22:37:02 +0100

    More tests

--- test/search.spec.js
+++ test/search.spec.js
@@ -1,12 +1,12 @@
 'use strict';
 
+var util = require('util');
 var raf = require('..')('key');
 var errors = require('../lib/resources/errors');
 var HTTPError = errors.HTTPError;
 var Q = require('q');
 
 describe('Search', function() {
-  // var params = {"flights": [{"from":"MXP","to":"DEN","departure":"2015-12-12","passengers":[{"category":"ADT"}]}]};
   describe('#query()', function() {
     describe('when there are no params', function() {
       var req = raf.search.query(); 
@@ -34,6 +34,28 @@ describe('Search', function() {
           return Q.all([
             message.should.have.deep.property('content.error'),
             message.content.message['obj.flights'][0].msg[0].should.equal('error.path.missing')
+          ]);
+        });
+      });
+    });
+
+    describe('when there are missing required params', function() {
+  // var params = {"flights": [{"from":"MXP","to":"DEN","departure":"2015-12-12","passengers":[{"category":"ADT"}]}]};
+      var req = raf.search.query({ flights: [
+        { from: 'SFO', departure: new Date() }
+      ]}); 
+
+      it('should return a 400 error', function() {
+        return req.should.be.rejectedWith(/400/);
+      });
+
+      it('should return a meaningful error message', function() {
+        return req.should.be.rejected.then(function(message) {
+          return Q.all([
+            message.should.have.deep.property('content.error'),
+            message.content.message['obj.flights[0].passengers'][0].msg[0].should.equal('error.path.missing'),
+            message.content.message['obj.flights[0].to'][0].msg[0].should.equal('error.path.missing')
+
           ]);
         });
       });
