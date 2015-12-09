dc991b15f4d433ca4c11bb102f447edf779900cf
Riccardo Gueli Alletti <riccardo.gueli.alletti@gmail.com>
2015-12-09 19:34:33 +0100

    E2E updated

--- test/e2e/flights.spec.js
+++ test/e2e/flights.spec.js
@@ -8,49 +8,52 @@ var logJSON = require('../support/log-json.js');
 var inspect      = require('util').inspect;
 var fs          = require('fs');
 
-describe('Flights', function() {
-  describe('#fetch()', function() {
-    describe('when it has a wrong flight id', function() {
-      var req;
+describe('E2E', function() {
+  describe('Flights', function() {
+    describe('#fetch()', function() {
+      describe('when it has a wrong flight id', function() {
+        var req;
 
-      before(function() {
-        req = yepjet.flights.fetch('foo');
-      });
+        before(function() {
+          req = yepjet.flights.fetch('foo');
+        });
 
-      it('should return a 404 error', function() {
-        return req.should.be.rejectedWith(/404/);
-      });
-    });
-
-    describe('when it has an existing flight id', function() {
-      var req;
-      var flightId;
-
-      before(function() {
-        req = yepjet.search.query({
-          flights: [
-            {
-              from: 'SFO',
-              to: 'JFK',
-              departure: moment().add(1, 'days').toDate()
-            }
-          ],
-          passengers: [{ category: 'ADT' }]
-        }).then(function(res) {
-          flightId = res['flights'][0][0].id;
-          return yepjet.flights.fetch(flightId);
+        it('should return a 404 error', function() {
+          return req.should.be.rejectedWith(/404/);
         });
       });
 
-      it('should return the selected flight', function() {
-        return req.should.be.eventually.fulfilled.then(function(res) {
-          return Q.all([
-            res.id.should.equal(flightId),
-            res.price.should.be.defined,
-            res.segments.should.not.be.empty
-          ]);
+      describe('when it has an existing flight id', function() {
+        var req;
+        var flightId;
+
+        before(function() {
+          req = yepjet.search.query({
+            flights: [
+              {
+                from: 'SFO',
+                to: 'JFK',
+                departure: moment().add(1, 'days').toDate()
+              }
+            ],
+            passengers: [{ category: 'ADT' }]
+          }).then(function(res) {
+            flightId = res['flights'][0][0].id;
+            return yepjet.flights.fetch(flightId);
+          });
+        });
+
+        it('should return the selected flight', function() {
+          return req.should.be.eventually.fulfilled.then(function(res) {
+            return Q.all([
+              res.id.should.equal(flightId),
+              res.price.should.be.defined,
+              res.segments.should.not.be.empty
+            ]);
+          });
         });
       });
     });
   });
 });
+
--- test/e2e/orders.spec.js
+++ test/e2e/orders.spec.js
@@ -8,65 +8,103 @@ var logJSON = require('../support/log-json.js');
 var inspect      = require('util').inspect;
 var fs          = require('fs');
 
-describe('Orders', function() {
-  var orderId;
-  var flightId;
+describe.only('E2E', function() {
+  describe('Orders', function() {
+    var orderId;
+    var flightId;
 
-  before(function() {
-    return yepjet.search.query({
-      flights: [
-        {
-          from: 'MAD',
-          to: 'FCO',
-          departure: moment().add(30, 'days').toDate()
-        }
-      ],
-      passengers: [{ category: 'ADT' }]
-    }).then(function(res) {
-      flightId = res.flights[0][0].id;
-    });
-  });
-
-  describe('#create()', function() {
-    describe('when there are no flights added', function() {
-      var req;
-
-      before(function() {
-        req = yepjet.orders.create();
-      });
-
-      it('should return an empty array of flights and an order id', function() {
-        return req.should.be.fulfilled.then(function(order) {
-          orderId = order.id;
-          return Q.all([
-            order.id.should.exist,
-            order.flights.should.be.empty
-          ]);
-        });
+    before(function() {
+      return yepjet.search.query({
+        flights: [
+          {
+            from: 'MAD',
+            to: 'FCO',
+            departure: moment().add(30, 'days').toDate()
+          }
+        ],
+        passengers: [{ category: 'ADT' }]
+      }).then(function(res) {
+        flightId = res.flights[0][0].id;
       });
     });
 
-    describe('when some flights are added', function() {
-      describe('and the ids are fake', function() {
+    describe('#create()', function() {
+      describe('when there are no flights added', function() {
         var req;
 
         before(function() {
-          req = yepjet.orders.create(['foo', { id: 'bar' }]);
+          req = yepjet.orders.create();
         });
 
-        it('should return a 400 error', function() {
-          return req.should.be.rejectedWith(/400/);
+        it('should return an empty array of flights and an order id', function() {
+          return req.should.be.fulfilled.then(function(order) {
+            orderId = order.id;
+            return Q.all([
+              order.id.should.exist,
+              order.flights.should.be.empty
+            ]);
+          });
         });
       });
 
-      describe('and the ids are valid', function() {
+      describe('when some flights are added', function() {
+        describe('and the ids are fake', function() {
+          var req;
+
+          before(function() {
+            req = yepjet.orders.create(['foo', { id: 'bar' }]);
+          });
+
+          it('should return a 400 error', function() {
+            return req.should.be.rejectedWith(/400/);
+          });
+        });
+
+        describe('and the ids are valid', function() {
+          var req;
+
+          before(function() {
+            req = yepjet.orders.create([flightId]).then(null, function(err) {
+              logJSON(err);
+            });
+          });
+
+          it('should return an array of flights with just the selected flight', function() {
+            return req.should.be.fulfilled.then(function(order) {
+              return Q.all([
+                order.id.should.exist,
+                order.flights.should.have.length(1),
+                order.flights[0].should.have.property('id').and.equal(flightId)
+              ]);
+            });
+          });
+        });
+      });
+    });
+
+    describe('#addFlight()', function() {
+      describe('when there is a fake flight id added', function() {
         var req;
 
         before(function() {
-          req = yepjet.orders.create([flightId]);
+          req = yepjet.orders.addFlight(orderId, 'fakeFlightId');
         });
 
-        it('should return an array of flights with just the selected flight', function() {
+        it('should return a 404 error', function() {
+          return req.should.be.rejectedWith(/404/);
+        });
+      });
+
+      describe('when there is a real flight id added', function() {
+        var req;
+
+        before(function() {
+          req = yepjet.orders.addFlight(orderId, flightId).then(null, function(err) {
+            logJSON(err);
+          });
+        });
+
+        it('should work', function() {
           return req.should.be.fulfilled.then(function(order) {
             return Q.all([
               order.id.should.exist,
@@ -78,36 +116,5 @@ describe('Orders', function() {
       });
     });
   });
-
-  describe('#addFlight()', function() {
-    describe('when there is a fake flight id added', function() {
-      var req;
-
-      before(function() {
-        req = yepjet.orders.addFlight(orderId, 'fakeFlightId');
-      });
-
-      it('should return a 404 error', function() {
-        return req.should.be.rejectedWith(/404/);
-      });
-    });
-
-    describe('when there is a real flight id added', function() {
-      var req;
-
-      before(function() {
-        req = yepjet.orders.addFlight(orderId, flightId);
-      });
-
-      it('should work', function() {
-        return req.should.be.fulfilled.then(function(order) {
-          return Q.all([
-            order.id.should.exist,
-            order.flights.should.have.length(1),
-            order.flights[0].should.have.property('id').and.equal(flightId)
-          ]);
-        });
-      });
-    });
-  });
 });
+
